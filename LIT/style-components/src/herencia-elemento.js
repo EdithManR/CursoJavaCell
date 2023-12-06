b5c2ef53afba7dcfe0ae4aab9331e086a434c55b
Edith MR <dariamovil@yahoo.com.mx>
2023-12-06 00:20:08 -0600

    se agrega style-components

--- LIT/style-components/src/herencia-elemento.js
+++ LIT/style-components/src/herencia-elemento.js
@@ -1,13 +1,18 @@
-import { LitElement, html, css } from 'lit-element';
-import { SuperElemento} from "./super-elemento";
+import { css } from 'lit-element';
+import { SuperElemento } from './super-elemento.js';
 
 export class HerenciaElemento extends SuperElemento {
-    static get styles(){
-        return [
-            super.styles,
-            css `button { color:white; background-color: blue; font-size: 20px;}`
-        ];
-    }
-
+  static get styles() {
+    return [
+      super.styles,
+      css`
+        button {
+          color: white;
+          background-color: blue;
+          font-size: 20px;
+        }
+      `,
+    ];
+  }
 }
 customElements.define('herencia-elemento', HerenciaElemento);
