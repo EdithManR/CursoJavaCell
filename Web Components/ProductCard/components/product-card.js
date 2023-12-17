ab9770c64714a17bbc5feb21f815862e61a27814
Edith MR <dariamovil@yahoo.com.mx>
2023-12-17 16:34:38 -0600

    estructura base del componente

--- Web Components/ProductCard/components/product-card.js
+++ Web Components/ProductCard/components/product-card.js
@@ -1,7 +1,33 @@
 class ProductCard extends HTMLElement{
   constructor(){
     super();
+    this.attachShadow({mode: "open"});
     console.log("Hola mundo");
+  }
+  getTemplate(){
+    const template =document.createElement('template');
+    template.innerHTML = `
+     <main>
+      <section>
+        <img/>
+      </section>
+      <section> 
+        <div>
+          <h2> </h2>
+          <p> texto </p>
+          <h3> </h3>
+          <button></button>
+        </div>
+      </section>
+     </main>
+    `;
+    return template;
+  }
+  render(){
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+  }
+  connectedCallback(){
+    this.render();
   }
 }
 
